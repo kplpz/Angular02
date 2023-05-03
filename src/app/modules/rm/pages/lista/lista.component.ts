35712959eb15641929072039a1b16a9344606447
Karen Lopez <pl15012@ues.edu.sv>
2023-05-02 22:18:25 -0600

    z

--- src/app/modules/rm/pages/lista/lista.component.ts
+++ src/app/modules/rm/pages/lista/lista.component.ts
@@ -1,10 +1,7 @@
 
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IPlace } from '@modules/rm/interface/placeh.interface';
-import { ISP } from '@modules/rm/interface/superheroes.interface';
 import { PlacehService } from '@modules/rm/service/placeh.service';
-// import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
 import { Router } from '@angular/router';
 
 
@@ -14,18 +11,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./lista.component.scss']
 })
 export class ListaComponent implements OnInit {
-  @ViewChild(MatPaginator, { static: true }) paginator !:MatPaginator
-  displayedColumns: string[] = ['position', 'image', 'name'];
 
   usuarios: IPlace[] = []
   usuariosString: string[] = []
-  //API SUPERHEROE
-  super: ISP[] = []
-  sa: string[] = []
-  //API POKEMON
-  pokemons = []
-  datos: any = []
-  // dataSource = new MatTableDataSource<any>(this.datos);
 
 
   constructor(
@@ -34,29 +22,7 @@ export class ListaComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    //throw new Error('Method not implemented.');
     this.usuarioPH()
-    //this.supp()
-    // this.getPokemons()
-  }
-
-
-  superHeroe(): void {
-    this.placeService.superHeroes.subscribe(resp => {
-      this.super = resp
-    })
-  }
-  supp(): void {
-    this.placeService.obtenerSuper().then(async (res: any) => {
-      res.forEach((o: any) => {
-        this.super.push(o)
-        this.sa.push(JSON.stringify(o))
-        console.log(this.sa)
-      })
-
-      let array = JSON.parse(this.sa[0])
-      console.log(array.name)
-    })
   }
 
   usuarioPH(): void {
@@ -69,42 +35,6 @@ export class ListaComponent implements OnInit {
       })
     })
   }
-
-  // PROBANDO API POKEMON V2
-  // getPokemons() {
-  //   let data
-
-  //   for (let i = 1; i < 150; i++) {
-  //     this.placeService.getPokemon(i).subscribe(res => {
-  //       data = {
-  //         position: i,
-  //         image: res.sprites.front_default,
-  //         name: res.name
-  //       }
-  //       // ESTO VIENE DEL SERVICE
-  //       this.datos.push(data)
-  //       this.dataSource = new MatTableDataSource<any>(this.datos)
-  //       this.dataSource.paginator = this.paginator
-  //     },
-  //       err => {
-  //         console.log(err)
-  //       })
-  //   }
-  // }
-
-  // applyFilter(event: Event) {
-  //   const filterValue = (event.target as HTMLInputElement).value;
-  //   this.dataSource.filter = filterValue.trim().toLowerCase();
-
-  //   if (this.dataSource.paginator) {
-  //     this.dataSource.paginator.firstPage();
-  //   }
-  // }
-
-  // getRow(row: any) {
-  //   //console.log(row);
-  //   this.router.navigateByUrl(`/pokeDetail/${row.position}`)
-  // }
 
 }
 
--- src/app/modules/rm/rm.module.ts
+++ src/app/modules/rm/rm.module.ts
@@ -5,10 +5,6 @@ import { RmRoutingModule } from './rm-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ListaComponent } from './pages/lista/lista.component';
 import { FormsModule } from '@angular/forms';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatPaginatorModule } from '@angular/material/paginator';
-// import { MatTableModule } from '@angular/material/table';
 
 const OPTIONS = {
   appearance: 'outline',
@@ -27,10 +23,6 @@ const OPTIONS = {
     FormsModule
   ],
   providers:[
-    {
-      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue:{...OPTIONS},
-    }
   ]
 })
 export class RmModule { }
